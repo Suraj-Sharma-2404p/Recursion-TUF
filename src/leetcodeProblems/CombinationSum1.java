86dbc0aa3e616a343009960e7a38ea4598a5933c
Suraj <surajsameer848506@gmail.com>
2023-07-21 09:59:11 +0530

    Update CombinationSum1.java

--- src/leetcodeProblems/CombinationSum1.java
+++ src/leetcodeProblems/CombinationSum1.java
@@ -1,12 +1,23 @@
 package leetcodeProblems;
 
 import java.util.ArrayList;
+
 class CombinationSum1 {
 
-    private void findCombinations(int ind, int[] arr, int target, ArrayList < ArrayList < Integer >> ans, ArrayList<Integer> ds) {
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 6, 7};
+        int target = 7;
+        CombinationSum1 sol = new CombinationSum1();
+        ArrayList<ArrayList<Integer>> ls = sol.combinationSum(arr, target);
+        System.out.println("Combinations are: ");
+        System.out.println(ls);
+
+    }
+
+    private void findCombinations(int ind, int[] arr, int target, ArrayList<ArrayList<Integer>> ans, ArrayList<Integer> ds) {
         if (ind == arr.length) {
             if (target == 0) {
-                ans.add(new ArrayList <>(ds));
+                ans.add(new ArrayList<>(ds));
             }
             return;
         }
@@ -17,18 +28,10 @@ class CombinationSum1 {
         }
         findCombinations(ind + 1, arr, target, ans, ds);
     }
-    public ArrayList < ArrayList < Integer >> combinationSum(int[] candidates, int target) {
-    	ArrayList < ArrayList < Integer >> ans = new ArrayList < > ();
-        findCombinations(0, candidates, target, ans, new ArrayList < > ());
+
+    public ArrayList<ArrayList<Integer>> combinationSum(int[] candidates, int target) {
+        ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+        findCombinations(0, candidates, target, ans, new ArrayList<>());
         return ans;
-    }
-    public static void main(String[] args) {
-        int arr[] = {2,3,6,7};
-        int target = 7;
-        CombinationSum1 sol = new CombinationSum1();
-        ArrayList < ArrayList < Integer >> ls = sol.combinationSum(arr, target);
-        System.out.println("Combinations are: ");
-        System.out.println(ls);
-        
     }
 }
